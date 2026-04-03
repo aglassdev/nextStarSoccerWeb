3a1ea453e6eebec0697cc33857ef620471c00c6e
aglassdev <amartyaglasses@gmail.com>
2026-04-03 19:13:35 -0400

    Image optimization fixes

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -5,7 +5,7 @@ export const images = {
   logo: '/assets/images/NextStarLogo.png',
   longLogo: '/assets/images/longLogo.png',
   masthead: '/assets/images/masthead.png',
-  homeHero: '/assets/images/homeHero.png', // Large hero image for home page
+  homeHero: '/assets/images/homeHero.webp', // Large hero image for home page
   
   // Add more images as needed
   coachesHeader: '/assets/images/coachesHeader.png',
